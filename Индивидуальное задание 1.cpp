a5237b22e9adff2a837fa0024b37d5436f962eb9
Fans1z <vitaka666@gmail.com>
2025-11-28 14:10:59 +0300

    Update Индивидуальное задание 1.cpp

--- Индивидуальное задание 1.cpp
+++ Индивидуальное задание 1.cpp
@@ -1,29 +1,16 @@
 // №2 - 1 Задание
 
-#include <iostream>
+#include <iostream> // Программа которая для заданного значения "X" вычесляет значение выражения
 #include <cmath>
-#include <iomanip>
-
 using namespace std;
 
 int main() {
     double x, y;
+    cout << "Введите x и y: ";
+    cin >> x >> y;
     
-    // Ввод переменных
-    cout << "Введите значение x: ";
-    cin >> x;
-    
-    cout << "Введите значение y: ";
-    cin >> y;
-    
-    // Вычисление выражения: e^(-x) - cos(x) + sin(2xy)
     double result = exp(-x) - cos(x) + sin(2 * x * y);
     
-    // Вывод результата
-    cout << "Результат вычисления e^(-x) - cos(x) + sin(2xy): " << endl;
-    cout << "При x = " << fixed << setprecision(2) << x << ", y = " << y << endl;
-    cout << "Результат = " << fixed << setprecision(6) << result << endl;
-    
+    cout << "Результат: " << result;
     return 0;
-
 }
