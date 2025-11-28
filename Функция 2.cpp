9ee7ab3eb8a7308a60d3f8a1cd3a77f1d0b2da56
Fans1z <vitaka666@gmail.com>
2025-11-28 12:51:14 +0300

    Add files via upload

---
+++ Функция 2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    double x, y;
+    
+    // Ввод переменных
+    cout << "Введите значение x: ";
+    cin >> x;
+    
+    cout << "Введите значение y: ";
+    cin >> y;
+    
+    // Вычисление выражения: e^(-x) - cos(x) + sin(2xy)
+    double result = exp(-x) - cos(x) + sin(2 * x * y);
+    
+    // Вывод результата
+    cout << "Результат вычисления e^(-x) - cos(x) + sin(2xy): " << endl;
+    cout << "При x = " << fixed << setprecision(2) << x << ", y = " << y << endl;
+    cout << "Результат = " << fixed << setprecision(6) << result << endl;
+    
+    return 0;
+}
