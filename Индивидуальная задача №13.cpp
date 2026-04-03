58cbe24e593c1cb0533fa906eae7e33d20cb61e7
Fans1z <vitaka666@gmail.com>
2026-04-03 09:54:45 +0300

    Add files via upload

---
+++ Индивидуальная задача №13.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char a;
+	cin >> a;
+	
+	if (a == 'a') cout << 250;
+	if (a == 'b') cout << 40;
+	if (a == 'm') cout << 280;
+	if (a == 'p') cout << 900;
+	if (a == 't') cout << 350;
+	
+	return 0;
+}
