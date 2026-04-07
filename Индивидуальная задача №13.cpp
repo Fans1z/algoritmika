9b7b6fa80bacc096033658221963bc3ebc318d6b
Fans1z <vitaka666@gmail.com>
2026-04-07 23:05:40 +0300

    Update Индивидуальная задача №13.cpp

--- Индивидуальная задача №13.cpp
+++ Индивидуальная задача №13.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main() {
-	char a;
-	cin >> a;
-	
-	if (a == 'a') cout << 250;
-	if (a == 'b') cout << 40;
-	if (a == 'm') cout << 280;
-	if (a == 'p') cout << 900;
-	if (a == 't') cout << 350;
-	
-	return 0;
+    char a;
+    cin >> a;
+
+    switch (a) {
+        case 'a': cout << 250; break;
+        case 'b': cout << 40; break;
+        case 'm': cout << 280; break;
+        case 'p': cout << 900; break;
+        case 't': cout << 350; break;
+    }
+
+    return 0;
 }
